ce0d767cad434d19c21e22fffa34d1c3a52fd511
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2023-09-28 17:53:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useEffect,useState } from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web'
+import NewsCards from './components/NewsCards';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+    const[newsArticles, setNewsArticles] = useState([]);
+
+    const alanKey = 'ca307562e7a2537f15277d25e9786be42e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+    useEffect(() => {
+        alanBtn({
+          key: alanKey,
+          onCommand: ({command, articles}) => {
+            if (command === 'newsHeadlines') {
+              setNewsArticles(articles);
+            }
+          },
+        });
+      }, []);
+    return (
+    <div>
+        ALan AI news Application
+      <NewsCards articles={newsArticles}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/NewsCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+// import NewsCards from './NewsCards'
+const NewsCard = () => {
+  return (
+    <div>NewsCard</div>
+  )
+}
+
+export default NewsCard
