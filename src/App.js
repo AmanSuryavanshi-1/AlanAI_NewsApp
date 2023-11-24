f52cbb9a96ce4f24fc07207f06e3a32c3b2780e0
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2023-11-24 13:30:33 +0530

    newsapp intigration

--- src/App.js
+++ src/App.js
@@ -5,16 +5,50 @@ import wordsToNumbers from 'words-to-numbers';
 import './styles.css'
 import img from './news.png'
 
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
+import Navbar from './components/Navbar/Navbar.jsx';
+import News from './components/AllNews/News.jsx';
+// const handleNavigation = (section) => {
+//   switch (section) {
+//       case 'home':
+//           window.location.href = '/';
+//           break;
+//       case 'entertainment':
+//           window.location.href = '/entertainment';
+//           break;
+//       case 'business':
+//           window.location.href = '/business';
+//           break;
+//       case 'health':
+//           window.location.href = '/health';
+//           break;
+//       case 'science':
+//           window.location.href = '/science';
+//           break;
+//       case 'sports':
+//           window.location.href = '/sports';
+//           break;
+//       case 'technology':
+//           window.location.href = '/technology';
+//           break;
+//       default:
+//           console.log('Invalid section:', section);
+//   }
+// };
+
 const App = () => {
     const[newsArticles, setNewsArticles] = useState([]);
     const[activeArticle, setActiveArticle] = useState(-1);
+    const apiKey = '3fd4a5e6fe19405489cc49ea3f5b8bcc';
     const alanKey = 'ca307562e7a2537f15277d25e9786be42e956eca572e1d8b807a3e2338fdd0dc/stage';
     // const alanKey ='b7c5003f6518b3a80b4d1ca9f98631012e956eca572e1d8b807a3e2338fdd0dc/stage';
     // const classes = useStyles();
     useEffect(() => {
         alanBtn({
           key: alanKey,
-          onCommand: ({command, articles, number}) => {
+          onCommand: ({command, articles, number, category}) => {
             if (command === 'newsHeadlines') {
               setNewsArticles(articles);
               setActiveArticle(-1);
@@ -32,16 +66,89 @@ const App = () => {
                }
               window.open(articles[number].url, '_blank');
             }
+          switch (command) {
+            case 'openNews':
+            handleOpenNews(category);
+            break;
+          case 'openHomePage':
+            // Handle the command to open the home page
+            handleOpenHomePage();
+            break;
+          // case'openEntertainment' :
+          //   handleOpenCategory('entertainment');
+          //   break;
+          // case 'openBusiness':
+          //   handleOpenCategory('business');
+          //   break;
+          // case 'openHealth':
+          //   handleOpenCategory('health');
+          //   break;
+          // case 'openScience':
+          //   handleOpenCategory('science');
+          //   break;
+          // case 'openSports':
+          //   handleOpenCategory('sports');
+          //   break;
+          // case 'openTechnology':
+          //   handleOpenCategory('technology');
+          //   break;
+      
+      //     break;
+          default:
+            break;
+        }
           },
         });
       }, []);
+      const handleOpenNews = (category) => {
+        window.location.href = `/${category.toLowerCase()}`;
+      };
+  // const handleOpenCategory = (category) => {
+  //   const encodedCategory = encodeURIComponent(category);
+  //   window.location.href = `/${encodedCategory}`;
+  // };
+  const handleOpenHomePage = () => {
+    // Perform actions to open the home page
+    // For example, redirect to the home route
+    window.location.href = '/';
+  };
+      const[progress, setProgress]= useState(0)
+      const pageSize=8;
+      const country='in';
+
     return (
     <div>
-      <div className="logoContainer">
+  
+      <BrowserRouter>
+<Navbar/>
+<LoadingBar
+        color='#f11946'
+        height={3}
+        progress={progress}
+      />
+
+      <div>
+        <Routes>
+        <Route exact path="/" element={<div><div className="logoContainer">
         <img src="https://cdn.dribbble.com/users/61571/screenshots/727897/news-app-icon1.png" className="newsLogo" alt="newsimg"/>
         <h1>AI NewsMate</h1>
-      </div>
-      <NewsCards articles={newsArticles} activeArticle={activeArticle}s/>
+        </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+    </div>}></Route>
+        {/* <Route exact path="/entertainment" element= {NewsCards ? (
+                  <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+                ) : (
+                  <News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country={country} category="entertainment" />
+                )}></Route> */}
+        <Route exact path="/entertainment" element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country={country} category="entertainment" />}></Route>
+        <Route exact path="/business" element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country={country} category="business"/>}></Route>
+        <Route exact path="/health" element={<News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country={country} category="health"/>}></Route>
+        <Route exact path="/science" element={<News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country={country} category="science"/>}></Route>
+        <Route exact path="/sports" element={<News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country={country} category="sports"/>}></Route>
+        <Route exact path="/technology" element={<News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country={country} category="technology"/>}></Route>
+        </Routes>
+        </div>
+ </BrowserRouter>
     </div>
   )
 }
--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,49 +1,112 @@
-import { Card,CardActionArea, CardActions, CardContent, CardMedia, Typography,Button } from '@material-ui/core'
-import React from 'react'
-// import classNames from 'classnames';
-import useStyles from './style'
-import { useState,useEffect, createRef } from 'react'
+import React from 'react';
+import { Card, CardActionArea, CardActions, CardContent, CardMedia, Typography, Button } from '@material-ui/core';
+import './style'; // Assuming you have a CSS file for additional styling
 
-const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, i, activeArticle }) => {
-  const classes = useStyles();
+const NewsItem = (props) => {
+  const { title, description, imageUrl, newsUrl, author, date, source, i, activeArticle } = props;
 
-  //implementing auto scroll
-  const [elementRefs, setElementRefs] = useState([]);
-  const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop -50);
-  // this is to set refs for all elements
-  useEffect(() => {
-    setElementRefs((refs) =>
-      Array(20)
-        .fill()
-        .map((_, j) => refs[j] || createRef()));
-  }, []);
+  const cardStyle = {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    position: 'absolute',
+    right: '0',
+  };
 
-  useEffect(() => {
-    if(i===activeArticle && elementRefs[activeArticle]){
-      scrollToRef(elementRefs[activeArticle]);
-    }
-  }, [i, activeArticle, elementRefs]);
+  const badgeStyle = {
+    backgroundColor: '#dc3545',
+    color: '#fff',
+    borderRadius: '20px',
+    padding: '5px 10px',
+  };
+
+  const card = {
+    height: '500px',
+  };
+
+  const image = {
+    height: '250px',
+  };
+
   return (
-    <div>
-      <Card ref={elementRefs[i]} className={ activeArticle === i ? classes.activeCard: classes.card}>
-        <CardActionArea href={url} target="_blank"> 
-          <CardMedia className={classes.media} image={urlToImage || 'https://i.ytimg.com/vi/3LgKoQByVQE/maxresdefault.jpg'}/>
-          <div className={classes.details}>
-            <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
-            <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
+    <div className={`my-3 ${activeArticle === i ? 'active' : ''}`}>
+      <Card className="card" style={card}>
+        <CardActionArea href={newsUrl} target="_blank">
+          <CardMedia className="card-img-top" style={image} image={imageUrl || 'https://i.ytimg.com/vi/3LgKoQByVQE/maxresdefault.jpg'} />
+          <div style={cardStyle}>
+            <span className="badge" style={badgeStyle}>
+              {source}
+            </span>
           </div>
-          <Typography className={classes.title} gutterBottom variant='h5'>{title}</Typography>
           <CardContent>
-            <Typography variant='body2' color="textSecondary" component="p">{description}</Typography>
+            <Typography variant="h5" component="h2">
+              {title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {description}...
+            </Typography>
           </CardContent>
         </CardActionArea>
-        <CardActions className={classes.CardActions}>
-        <Button size="small" color="primary">Read More</Button>
-        <Typography variant='h5' color='textSecondary'>{i+1}</Typography>
+        <CardActions>
+          <Button size="small" color="primary" href={newsUrl} target="_blank">
+            Read More
+          </Button>
+          <Typography variant="h5" color="textSecondary">
+            {i + 1}
+          </Typography>
         </CardActions>
       </Card>
     </div>
-  )
-}
+  );
+};
 
-export default NewsCard
+export default NewsItem;
+
+// import { Card,CardActionArea, CardActions, CardContent, CardMedia, Typography,Button } from '@material-ui/core'
+// import React from 'react'
+// // import classNames from 'classnames';
+// import useStyles from './style'
+// import { useState,useEffect, createRef } from 'react'
+
+// const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, i, activeArticle }) => {
+//   const classes = useStyles();
+
+//   //implementing auto scroll
+//   const [elementRefs, setElementRefs] = useState([]);
+//   const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop -50);
+//   // this is to set refs for all elements
+//   useEffect(() => {
+//     setElementRefs((refs) =>
+//       Array(20)
+//         .fill()
+//         .map((_, j) => refs[j] || createRef()));
+//   }, []);
+
+//   useEffect(() => {
+//     if(i===activeArticle && elementRefs[activeArticle]){
+//       scrollToRef(elementRefs[activeArticle]);
+//     }
+//   }, [i, activeArticle, elementRefs]);
+//   return (
+//     <div>
+//       <Card ref={elementRefs[i]} className={ activeArticle === i ? classes.activeCard: classes.card}>
+//         <CardActionArea href={url} target="_blank"> 
+//           <CardMedia className={classes.media} image={urlToImage || 'https://i.ytimg.com/vi/3LgKoQByVQE/maxresdefault.jpg'}/>
+//           <div className={classes.details}>
+//             <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
+//             <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
+//           </div>
+//           <Typography className={classes.title} gutterBottom variant='h5'>{title}</Typography>
+//           <CardContent>
+//             <Typography variant='body2' color="textSecondary" component="p">{description}</Typography>
+//           </CardContent>
+//         </CardActionArea>
+//         <CardActions className={classes.CardActions}>
+//         <Button size="small" color="primary">Read More</Button>
+//         <Typography variant='h5' color='textSecondary'>{i+1}</Typography>
+//         </CardActions>
+//       </Card>
+//     </div>
+//   )
+// }
+
+// export default NewsCard
