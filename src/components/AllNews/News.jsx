593c9e4aa209533994dbb2266932a5a8f404185b
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2023-12-17 23:23:09 +0530

    17/12

--- src/components/AllNews/News.jsx
+++ src/components/AllNews/News.jsx
@@ -100,3 +100,116 @@ News.propTypes = {
 };
 
 export default News;
+
+// import React, { useEffect, useState } from 'react';
+// import NewsItem from '../NewsItem/NewsItem';
+// import PropTypes from 'prop-types';
+// // import InfiniteScroll from 'react-infinite-scroll-component';
+// import './News.css';
+
+// const News = (props) => {
+//   const capitalizeFirstLetter = (string) => {
+//     return string.charAt(0).toUpperCase() + string.slice(1);
+//   };
+
+//   const [articles, setArticles] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   // const [page, setPage] = useState(1);
+//   const [totalResults, setTotalResults] = useState(0);
+
+//   const updateNews = async () => {
+//     props.setProgress(10);
+//     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&pageSize=12`;
+//     try {
+//       setLoading(true);
+//       let res = await fetch(url);
+//       props.setProgress(30);
+  
+//       if (!res.ok) {
+//         throw new Error(`HTTP error! Status: ${res.status}`);
+//       }
+  
+//       let data = await res.json();
+//       props.setProgress(50);
+  
+//       if (data.articles) {
+//         setArticles(data.articles);
+//         setTotalResults(data.totalResults);
+//       } else {
+//         console.error("Received data structure is unexpected:", data);
+//       }
+  
+//       setLoading(false);
+//       props.setProgress(100);
+//     } catch (e) {
+//       console.error("Something went wrong:", e);
+//       setLoading(false);
+//     }
+//   };
+
+//   useEffect(() => {
+//     document.title = `${capitalizeFirstLetter(props.category)} - AI NewsMate`;
+//     updateNews();
+//     // eslint-disable-next-line
+//   }, []);
+
+//   // const fetchMoreData = async () => {
+//   //   const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page + 1}&pageSize=${props.pageSize}`;
+//   //   setPage(page + 1);
+//   //   try {
+//   //     let res = await fetch(url);
+//   //     let data = await res.json();
+
+//   //     setArticles(articles.concat(data.articles));
+//   //     setTotalResults(data.totalResults);
+//   //   } catch (e) {
+//   //     console.log("Something is not working");
+//   //   }
+//   // };
+
+//   return (
+//     <>
+//       <h1 className="text-center">
+//         AI NewsMate : Top {capitalizeFirstLetter(props.category)} Headlines
+//       </h1>
+
+//       {/* <InfiniteScroll
+//         dataLength={articles.length}
+//         next={fetchMoreData}
+//         hasMore={articles.length !== totalResults}
+//       > */}
+//         <div className="main">
+//           {articles.map((element) => {
+//             return (
+//               <div className="newsItem" key={element.url}>
+//                 <NewsItem
+//                   title={element.title ? element.title.slice(0, 80) : ' '}
+//                   description={element.description ? element.description.slice(0, 180) : ' '}
+//                   imageUrl={element.urlToImage}
+//                   newsUrl={element.url}
+//                   author={element.author}
+//                   date={element.publishedAt}
+//                   source={element.source.name}
+//                 />
+//               </div>
+//             );
+//           })}
+//         </div>
+//       {/* </InfiniteScroll> */}
+//     </>
+//   );
+// };
+
+// News.defaultProps = {
+//   country: 'in',
+//   // pageSize: 8,
+//   category: 'general',
+// };
+
+// News.propTypes = {
+//   country: PropTypes.string,
+//   // pageSize: PropTypes.number,
+//   category: PropTypes.string,
+// };
+
+// export default News;
